fc709ea2a103d1f3309cc9627283c2c903b6b0ee
Suellen Karolainy <103966382+SuKarolainy@users.noreply.github.com>
2024-03-22 08:05:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o primeiro numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a operação desejada: + , - , * , / ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+': 
+                    result = num1 + num2; break;
+                case '-':
+                    result = num1 - num2; break;
+                case '*':
+                    result = num1 * num2; break;
+                case '/':
+                    result = num1 / num2; break;
+
+            }
+
+            Console.WriteLine($"O resultado é: {result} ");
+        }
+    }
+}
